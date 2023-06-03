ea0b3cd843a42b5a5e6bc517d55b23226a4c5a05
Almarhoon Ibraheem <ibeem@outlook.com>
2023-06-03 22:14:27 +0300

    update version number

--- storage/.config/custom_start.sh
+++ storage/.config/custom_start.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env sh
 emuelec-bluetooth 15 -1 &
+
+# write version number
+cd /storage/roms/retroverse
+current_version=$(git rev-list --count HEAD)
+echo "1.0$current_version" >/storage/.config/EE_VERSION
